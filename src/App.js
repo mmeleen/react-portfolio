1d738a65a9866bd90bbac612bc4a960169d1d8bd
Matt Meleen <mmeleen@utexas.edu>
2020-08-28 21:50:31 -0500

    Commented out contact page

--- src/App.js
+++ src/App.js
@@ -21,9 +21,9 @@ function App() {
           <Route exact path="/portfolio">
             <Portfolio />
           </Route>
-          <Route exact path="/contact">
+          {/* <Route exact path="/contact">
             <Contact />
-          </Route>
+          </Route> */}
           <Route>
             <Redirect to="/about" />
           </Route>
