05c322e83185da4d4781869f5c3fe4e94a5cd627
Matt Meleen <mmeleen@utexas.edu>
2020-08-28 22:09:50 -0500

    Updates

---
+++ precache-manifest.92990998f6db8210f155bf235fecae38.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bff5aaa78f099fe5dad",
+    "url": "/react-portfolio/static/js/main.ec50bba5.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "2b5148d5d03d3403f7c0",
+    "url": "/react-portfolio/static/js/2.c5ff2196.chunk.js"
+  },
+  {
+    "revision": "4f482e835bf3407362b87e83e6d7c484",
+    "url": "/react-portfolio/index.html"
+  }
+];
