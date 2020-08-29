374a2294079944fa0be7e4a7b5c4cb4f77eaf4d3
Matt Meleen <mmeleen@utexas.edu>
2020-08-29 15:40:23 -0500

    updated portfolio page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,13 +3,15 @@ import React from "react";
 function Project(props) {
   return (
     <>
-      <div class="row border-secondary border-bottom mt-3 mb-3">
-        <div class="col-sm-4 text-center text-sm-left">
-          <img src={props.src} alt={props.alt} width="200" height="200" class="img-fluid img-thumbnail mt-2 mb-3"></img>
+      <div className="row border-secondary border-bottom mt-3 mb-3">
+        <div className="col-sm-4 text-center text-sm-left">
+          <img src={props.src} alt={props.alt} width="200" height="200" className=" img-thumbnail mt-2 mb-3"></img>
         </div>
-        <div class="col-sm-8 text-center text-sm-left">
-          <a href={props.url} class="lead text-info">{props.title}</a>
-          <p class="mt-2">{props.summary}</p>
+        <div className="col-sm-8 text-center text-sm-left">
+          <h4 className="lead text-info">{props.title}</h4>
+          <p className="mt-2">{props.summary}</p>
+          <a href={props.url} className="text-muted">Visit the Deployed Project</a><span className="card-text"> or </span>
+          <a href={props.gitUrl} className="text-muted">Visit the GitHub Repository</a>
         </div>
       </div>
     </>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -10,12 +10,12 @@ function Portfolio() {
             <h3>Portfolio</h3>
           </div>
         </div>
-        <Project src="http://placehold.it/200x200" alt="Proj1" url="#" title="PrePoll" summary="Political information app to provide information regarding a user's local elections and demographics." />
-        <Project src="http://placehold.it/200x200" alt="Proj2" url="#" title="Scope" summary="Astrology app that provides horoscopes and compatibility information." />
-        <Project src="http://placehold.it/200x200" alt="Proj3" url="#" title="Employee Directory - React" summary="Table of employees that can be easily filtered and sorted by search term or column." />
-        <Project src="http://placehold.it/200x200" alt="Proj4" url="#" title="Note Taker" summary="Create, store, update, and delete notes." />
-        <Project src="http://placehold.it/200x200" alt="Proj5" url="#" title="Before You Buy" summary="Provides a snapshot of information about a given video game to a prospective buyer." />
-        <Project src="http://placehold.it/200x200" alt="Proj6" url="#" title="Weather Dashboard" summary="Current and 5-day weather forecasts using the OpenWeatherMap API." />
+        <Project src="https://i.imgur.com/ZjZxiOE.png" alt="PrePoll" url="https://prepoll.herokuapp.com/" gitUrl="https://github.com/Kinzito17/PrePoll" title="PrePoll" summary="Political information app to provide information regarding a user's local elections and demographics." />
+        <Project src="https://i.imgur.com/oR5bo4V.png" alt="Scope" url="https://scope-g1.herokuapp.com/" gitUrl="https://github.com/mmeleen/scope" title="Scope" summary="Astrology app that provides horoscopes and compatibility information." />
+        <Project src="https://i.imgur.com/JMkYVIN.png" alt="Employee Directory" url="https://mmeleen.github.io/employee-directory_react/" gitUrl="https://github.com/mmeleen/employee-directory_react" title="Employee Directory - React" summary="Table of employees that can be easily filtered and sorted by search term or column." />
+        <Project src="https://i.imgur.com/SUGXdHV.png" alt="Note Taker" url="https://note-taker-express-m.herokuapp.com/" gitUrl="https://github.com/mmeleen/note-taker_express" title="Note Taker" summary="Create, store, update, and delete notes." />
+        <Project src="https://i.imgur.com/DJwLhgE.png" alt="Before You Buy" url="https://mmeleen.github.io/project1-g5/" gitUrl="https://github.com/mmeleen/project1-g5" title="Before You Buy" summary="Provides a snapshot of information about a given video game to a prospective buyer." />
+        <Project src="https://i.imgur.com/wmxRBso.png" alt="Weather Dashboard" url="#https://mmeleen.github.io/weather-dashboard_server-side-api/" gitUrl="https://github.com/mmeleen/weather-dashboard_server-side-api" title="Weather Dashboard" summary="Current and 5-day weather forecasts using the OpenWeatherMap API." />
         <div><br></br></div>
       </main>
     </>
