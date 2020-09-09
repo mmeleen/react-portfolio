d626d2521be2a4d874d115a40f1a111ae220266d
Matt Meleen <mmeleen@utexas.edu>
2020-09-08 21:29:07 -0500

    Updates

---
+++ precache-manifest.2cf17517ba6091f4b7d99253df87f900.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f020688dec92578ca6a",
+    "url": "/static/js/main.5c9805c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b5148d5d03d3403f7c0",
+    "url": "/static/js/2.c5ff2196.chunk.js"
+  },
+  {
+    "revision": "36947fdb72cd9a98a138fa4d70d01306",
+    "url": "/index.html"
+  }
+];
